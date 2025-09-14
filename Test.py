6bbfc43c4bb85529d643ae0334a97f240863cece
ManthanBro1 <manthagadiya8881@gmail.com>
2025-09-14 16:10:34 +0530

    Initial commit

---
+++ Test.py
@@ -0,0 +1,13 @@
+FEWSHOTEXAMPLES = [
+    {
+        "name": "string",
+        "email": "string",
+        "skills": ["string"],
+        "experience": [
+            {"company": "string", "role": "string", "years": "number"}
+        ]
+    }
+]
+man=""
+man+=str(FEWSHOTEXAMPLES)
+print(man)
